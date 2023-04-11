a46f9ef956d8bb4f40e55b67e724418c3dbafa1a
AdhikariM <maheshadhikari@live.com>
2023-04-10 23:17:52 -0500

    utility class

--- src/test/java/SharedUtilities.java
+++
@@ -1,9 +0,0 @@
-import io.appium.java_client.AppiumBy;
-import org.testng.annotations.Test;
-
-public class SharedUtilities extends AppiumConfiguration{
-    @Test
-    public void basicTest(){
-        driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
-    }
-}
