64a18e658723a2c8efa9a719bff4df2e7f8bb052
AdhikariM <maheshadhikari@live.com>
2023-05-17 21:09:19 -0500

    fix

--- src/test/java/PickerViewTests.java
+++ src/test/java/PickerViewTests.java
@@ -24,4 +24,21 @@ public class PickerViewTests extends AppiumConfiguration {
         PickerView pickerView = homepage.colorMixer();
         pickerView.colorMixer();
     }
+
+    @Test
+    public void printNumberOfElementsInPickerView() {
+        // Initialize the page object
+        PickerView pickerView = homepage.colorMixer();
+
+//        // Wait for the color mixer to be visible
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.visibilityOf(pickerView.getColorMixer()));
+
+        // Get the number of elements in the picker view
+        int numberOfElements = pickerView.getNumberOfPickerViewElements();
+
+        // Print the number of elements
+        System.out.println("Number of elements in the picker view: " + numberOfElements);
+    }
+
 }
